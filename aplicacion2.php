8d5a7f15063b0db130ca421a8159deb957f14df9
lazaro <107058297+l-lazarogt@users.noreply.github.com>
2023-10-19 03:51:35 -0600

    Add files via upload

---
+++ aplicacion2.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <style type="text/css">
+        body {
+            margin: 0;
+            background: url("https://i.postimg.cc/SK7wym5j/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg") no-repeat center center fixed;
+            background-size: cover;
+        }
+
+        /* Establece el ancho máximo para evitar que el contenido se estire demasiado en pantallas grandes. */
+        max-width: 1200px;
+        margin: 0 auto; /* Centra el contenido en la página */
+    }
+
+    /* Agrega estilos específicos para pantallas más pequeñas usando @media queries */
+    @media (max-width: 768px) {
+        /* Estilos para pantallas de hasta 768px de ancho */
+        body {
+            font-size: 16px;
+        }
+    }
+
+    /* Estilos para pantallas más grandes */
+    @media (min-width: 1200px) {
+        body {
+            font-size: 20px;
+        }
+    }
+
+    <!--
+#Layer1 {
+	position:absolute;
+	left:0;
+	top:0;
+	width:100%;
+	height:50px;
+	z-index:1;
+	
+}
+#Layer2 {
+	position:absolute;
+	left:0;
+	top:451px;
+	width:100%;
+	height:63px;
+	z-index:2;
+	background-color: #FF0000;
+	visibility: visible;
+}
+.TAMA_300 {	max-width:300PX:
+}
+.cuadro {border: 0px solid #666666;
+  font-size: 15px;
+  font-family: Arial, Verdana;
+  padding-left: 3px;
+  padding-right: 3px;
+  padding-top: 3px;
+  padding-bottom: 3px;
+  border-radius: 30px;
+  color: #2E3133;
+  background-color: #FFFFFF;
+}
+.cuadro1 {border: 0px solid #666666;
+  font-size: 15px;
+  font-family: Arial, Verdana;
+  padding-left: 3px;
+  padding-right: 3px;
+  padding-top: 3px;
+  padding-bottom: 3px;
+  border-radius: 30px;
+  color: #2E3133;
+  background-color: #FFFFFF;
+}
+.cuadro2 {border: 0px solid #666666;
+  font-size: 15px;
+  font-family: Arial, Verdana;
+  padding-left: 3px;
+  padding-right: 3px;
+  padding-top: 3px;
+  padding-bottom: 3px;
+  border-radius: 30px;
+  color: #2E3133;
+  background-color: #FFFFFF;
+}
+
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   color: white;
+   text-align: center;
+   z-index:1;
+   background-color: #333333;
+}
+
+.whatsapp {
+  color: white;
+  border: none;
+  cursor: pointer;
+  position: fixed;
+  bottom: 100px;
+  width: 100px;
+  right:20px;
+}
+
+
+
+.redes_sociales {  font-size: 15px;
+  font-family: Arial, Verdana;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border-radius: 30px;
+  color: #2E3133;
+   position: fixed;
+   bottom: 50px;
+   width: 100%;
+   text-align: center;
+   z-index:1;
+}
+.textbox {
+
+border: 1px solid #CCCCCC;
+  font-family: Arial, Verdana;
+  border-radius: 25px;
+  color: #1A019B;
+  text-decoration:none;
+}
+;
+  
+}
+.Estilo6 {color: #FFCC00}
+
+/*************************************/
+
+.sidebar {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #FFFFFF;
+  overflow-x: hidden;
+  transition: 0.5s;
+}
+
+
+.sidebar a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidebar a:hover {
+  color: #f1f1f1;
+}
+
+.sidebar .closebtn {
+  position: absolute;
+  top: 10;
+  right: 10px;
+  font-size: 36px;
+  margin-left: 30px;
+}
+
+.openbtn {
+  font-size: 20px;
+  cursor: pointer;
+  background-color: #111;
+  color: white;
+  padding: 1px 15px;
+  border: none;
+  
+}
+
+.openbtn:hover {
+  background-color: #444;
+}
+
+#main {
+  transition: margin-left .5s;
+  padding: 16px;
+
+}
+</style>
+</head>
+<body style="margin: 0">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+            <td valign="top">
+                <table width="100%" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="50%">&nbsp;</td>
+        <td width="50%" align="right"><div id="mySidebar" class="sidebar">
+          <a href="javascript:void(0)" class="closebtn" onClick="closeNav()"><img src="https://i.postimg.cc/FF6SLsCz/X.png" width="20" height="20"></a>
+  
+  <table width="100%" border="0" cellpadding="0" cellspacing="0">
+    <tr>
+      <td><img src="IMG/banner/banner.png" width="100%" height="auto"></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><table width="100%" border="0" cellpadding="0" cellspacing="10">
+        <tr>
+          <td><a href="aplicacion.php"><img src="https://i.postimg.cc/CL6h94cc/inicio.png" width="100%" height="auto" border="0"></a></td>
+        </tr>
+        <tr>
+          <td><a href="tel:+50255971879"><img src="https://i.postimg.cc/2yR9KyJk/Contactenos.png" width="100%" height="auto" border="0"></a></td>
+        </tr>
+      </table></td>
+    </tr>
+    <tr>
+      <td><hr></td>
+    </tr>
+    <tr>
+      <td><table width="100%" border="0" cellpadding="0" cellspacing="10">
+
+        <tr>
+          <td><a href="https://www.facebook.com/profile.php?id=100072261427908&mibextid=ZbWKwL"><img src="https://i.postimg.cc/jj21WGPb/facebook-1.png" width="100%" height="auto" border="0"></td>
+        </tr>
+        <tr>
+          <td><a href=""><img src="https://i.postimg.cc/pLq7L29C/instagram-1.png" width="100%" height="auto" border="0"></td>
+        </tr>
+        <tr>
+          <td><a href="https://wa.me/50255971879"><img src="https://i.postimg.cc/x10hQkdq/whatsapp-1.png" width="100%" height="auto" border="0"></td>
+        </tr>
+        <tr>
+          <td><a href="https://youtube.com/@midiosmitodooficial9804?si=qTeNzoYOmQFA4LSq"><img src="https://i.postimg.cc/25tKn27B/Youtube-1.png" width="100%" height="auto" border="0"></td>
+        </tr>
+      </table></td>
+    </tr>
+    <tr>
+      <td><hr></td>
+    </tr>
+    <tr>
+      <td><table width="100%" border="0" cellpadding="0" cellspacing="10">
+        <tr>
+          <td> <activity android:name=".MainActivity">
+        <intent-filter>
+            <action android:name="android.intent.action.MAIN" />
+
+            <category android:name="android.intent.category.LAUNCHER" />
+        </intent-filter>
+
+        <intent-filter>
+            <action android:name="android.intent.action.SEND" />
+            <category android:name="android.intent.category.DEFAULT" />
+            <data android:mimeType="text/plain" />
+        </intent-filter>
+
+    </activity>&nbsp;<img src="IMG/MENU/desarrollado por pideelo.png" width="80%" height="auto"></td>
+            </tr>
+        <tr>
+          <td></td>
+        </tr>
+
+      </table></td>
+    </tr>
+  </table>
+  </div>
+
+<div id="main">
+    <button class="openbtn" onClick="toggleNav()">☰</button>
+
+<script>
+let sidebarOpen = false;
+
+function toggleNav() {
+    if (sidebarOpen) {
+        closeNav();
+    } else {
+        openNav();
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    sidebarOpen = true;
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    sidebarOpen = false;
+}
+</script></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="500" valign="top">
+	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://mejorenlaradio.com/player/luna/js/lunaradio.min.js"></script>
+        <div id="lunaradio" style="  height:400px;"></div>
+      
+	  
+    <script>
+     jQuery(document).ready(function($) {
+	 $("#lunaradio").lunaradio({
+	    userinterface: "big",
+		backgroundcolor: "transparent",
+		fontcolor: "#FFFFFF",
+		hightlightcolor: "#FFFFFF",
+		fontname: "Saira Condensed",
+	    googlefont: "Saira Condensed:wght@100",
+		fontratio: "1.0",
+		radioname: "Mi Dios Mi Todo",
+		scroll: "true",
+		coverimage: "https://i.postimg.cc/pX8YbWkR/german.png",
+		onlycoverimage: "false",
+		coverstyle: "animated",
+		usevisualizer: "real",
+		visualizertype: "8",
+		multicolorvisualizer: "false",			
+		visualizeropacity: "0.0",
+		usestreamcorsproxy: "false", //true o false
+corsproxy: "https://evsportugal.herokuapp.com/?q=",
+		itunestoken: "1000lIPN",
+		metadatatechnic: "ownmetadataurl",
+        ownmetadataurl: "https://status.rcast.net/69947",
+        metadata: "",
+        streamurl: "https://stream.rcast.net/69947",
+        streamtype: "shoutcast2",
+		icecastmountpoint: "/live",
+        streamsuffix: "",
+		shoutcastpath: "/stream",
+        shoutcastid: "1",
+		metadatainterval: "20000",
+		volume: "100",
+		debug: "false",
+		autoplay: "true",
+		displayliveicon: "true",
+		displayvisualizericon: "true",
+     });
+  });
+    </script></td>
+  </tr>
+                </table>
+                <img src="https://i.postimg.cc/SK7wym5j/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg" width="100%" height="auto"></img>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
